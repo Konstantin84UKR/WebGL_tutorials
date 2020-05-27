7e0cce271026e7ffa96eea41f912a582abe30df3
Бондаренко Костянтин <Bondarenko.KI@blg.local>
2020-05-27 15:40:37 +0300

    camera

--- Normal_Map/Camera.js
+++ Normal_Map/Camera.js
@@ -53,13 +53,46 @@ class Camera {
 
 class CameraController {
     constructor(c, gui) {
-
+        let oThis = this;
         this.camera = c;
         this.gui = gui;
+
+        document.addEventListener("keydown", function (e) { oThis.onkeydown(e); });
+        document.addEventListener("keyup", function (e) { oThis.onkeyup(e); });
+        document.addEventListener("keypress", function (e) { oThis.onkeypress(e); });
+
+        this.position = glMatrix.vec3.create();
+        this.position.x = this.gui.view_directionX;
+        this.position.z = this.gui.view_directionZ;
     }
 
     updateMatrix() {
-        glMatrix.mat4.lookAt(this.camera.vMatrix, [this.gui.view_directionX, this.gui.view_directionY, this.gui.view_directionZ], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]);
+        // glMatrix.mat4.lookAt(this.camera.vMatrix, [this.gui.view_directionX, this.gui.view_directionY, this.gui.view_directionZ], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]);
+        glMatrix.mat4.lookAt(this.camera.vMatrix, [this.position.x, this.gui.view_directionY, this.position.z], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]);
+    }
+
+    onkeydown = function (e) {
+
+        // if (e.key === "ArrowUp") { this.dZ = 0.03; }
+        // if (e.key === "ArrowDown") { this.dZ = -0.03; }
+
+    };
+
+    onkeyup = function (e) {
+
+        // if (e.key === "ArrowUp") { this.dZ = 0.0; }
+        // if (e.key === "ArrowDown") { this.dZ = 0.0; }
+
+    }
+
+    onkeypress = function (e) {
+
+        if (e.key === "d") { this.position.x += 0.1; }
+        if (e.key === "a") { this.position.x -= 0.1; }
+
+        if (e.key === "w") { this.position.z -= 0.1; }
+        if (e.key === "s") { this.position.z += 0.1; }
+
     }
 
 }
