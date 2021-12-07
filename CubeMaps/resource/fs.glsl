f28b1187e02566f4c5ede50d446728de729f08e7
Константин Бондаренко <k.bondarenko@argo.ua>
2021-12-07 19:47:25 +0200

    cubemaps

---
+++ CubeMaps/resource/fs.glsl
@@ -0,0 +1,22 @@
+#version 300 es
+precision highp float;
+
+layout(std140, column_major) uniform;
+
+in vec3 v_normal;
+in vec2 v_uv;
+
+uniform sampler2D samplerTex;
+uniform mat4 u_Vmatrix;
+
+layout(location = 0) out vec4 fragFragColor;
+
+void main() {
+
+    vec2 uv = v_uv * 2.0;
+    // Move normal to view space
+    vec2 muv = vec2(u_Vmatrix * vec4(normalize(v_normal), 0)) * 0.5 + vec2(0.5, 0.5);
+    // read texture inverting Y value
+    // gl_FragColor = texture2D(samplerTex, vec2(muv.x, 1.0 - muv.y));
+    fragFragColor = texture(samplerTex, vec2(muv.x, muv.y)); //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL,true);
+}
